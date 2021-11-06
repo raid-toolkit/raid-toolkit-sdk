ad9db28627f558766db3f0691b069c80ae64d297
Daniel Chattan <dnchattan@gmail.com>
2021-11-06 09:49:59 -0400

    fix initial install behavior

--- SDK/Service/Actions/RunAction.cs
+++ SDK/Service/Actions/RunAction.cs
@@ -37,7 +37,7 @@ namespace Raid.Service
             if (Environment.GetEnvironmentVariable("RTK_DEBUG") != "true" && parentDirName != "win-x64")
             {
                 string expectedInstallPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "RaidToolkit");
-                string exeName = Path.GetFileName(AppConfiguration.ExecutablePath);
+                string exeName = ThisAssembly.AssemblyName;
                 string expectedExePath = Path.Join(expectedInstallPath, exeName);
                 string expectedConfigPath = Path.Join(expectedInstallPath, "appsettings.json");
                 if (!AppConfiguration.ExecutablePath.Equals(expectedExePath, StringComparison.OrdinalIgnoreCase))
