9244430afecc9e8a23f2a28256c5f9014851c495
Daniel Chattan <dnchattan@gmail.com>
2021-11-07 17:29:29 -0500

    auto-register for startup

--- SDK/Service/Services/MainService.cs
+++ SDK/Service/Services/MainService.cs
@@ -84,7 +84,10 @@ namespace Raid.Service
         public void Run(RunOptions options)
         {
             if (!options.Standalone)
+            {
                 RegisterAction.RegisterProtocol(true);
+                RegisterAction.RegisterStartup(true);
+            }
 
             Console.CancelKeyPress += (sender, e) => Application.Exit();
             TaskExtensions.RunAfter(1, UpdateAccounts);
