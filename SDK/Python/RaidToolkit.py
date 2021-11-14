d7c558e36bef641f8c30956ec431bf36b992c6e3
Daniel Chattan <dnchattan@gmail.com>
2021-11-14 13:46:55 -0500

    kill me now

--- SDK/Python/RaidToolkit.py
+++ SDK/Python/RaidToolkit.py
@@ -1,39 +1,48 @@
+import asyncio
 from typing import Dict
 import websocket
-from uuid import UUID, uuid4
+from uuid import uuid4
 from threading import Thread
 from promise import Promise
 import json
+import time
 
 
 class PromiseStore:
     """Manages async promise IDs"""
-    Promises: Dict[UUID, Promise] = {}
 
-    @classmethod
+    def __init__(self):
+        self.Promises: Dict[str, any] = {}
+
     def create(self):
-        id = uuid4()
-        self.Promises[id] = Promise()
+        id = str(uuid4())
 
-    @classmethod
-    def complete(self, id: UUID, value: any):
-        self.Promises[id].resolve(value)
+        def executor(resolve, reject):
+            self.Promises[id] = {
+                "id": id,
+                "resolve": resolve,
+                "reject": reject,
+            }
+        p = Promise(executor)
+        self.Promises[id]["promise"] = p
+        return id
 
-    @classmethod
-    def fail(self, id: UUID, error: Exception):
-        self.Promises[id].reject(error)
+    def complete(self, id: str, value: any):
+        self.Promises[id]["resolve"](value)
 
-    @classmethod
-    def get(self, id: UUID):
-        return self.Promises[id]
+    def fail(self, id: str, error: any):
+        self.Promises[id]["reject"](error)
+
+    def get(self, id: str):
+        return self.Promises[id]["promise"]
 
 
 class RaidToolkitClient:
     """Provides access to Raid Toolkit APIs"""
 
     def __init__(self, endpointUri="ws://localhost:9090"):
-        self.Promises = PromiseStore
-        self.Connected = Promise()
+        self.Promises = PromiseStore()
+        self.Connected = False
         self.ws = websocket.WebSocketApp(endpointUri,
                                          on_message=self._on_message,
                                          on_error=self._on_error,
@@ -44,9 +53,11 @@ class RaidToolkitClient:
             self.ws.run_forever()
 
         Thread(target=run).start()
-        Promise.wait(self.Connected, 1000)
 
-    def call(self, apiName: str, methodName: str, args=[]):
+    def call(self, apiName: str, methodName: str, args=[]) -> Promise[any]:
+        while self.Connected == False:
+            time.sleep(0.01)
+
         promiseId = self.Promises.create()
         self.ws.send(json.dumps([
             apiName, "call", {
@@ -55,24 +66,40 @@ class RaidToolkitClient:
                 "args": args,
             }
         ]))
+        return self.Promises.get(promiseId)
 
     def _on_open(self, ws):
-        self.Connected.resolve(ws)
+        self.Connected = True
         return
 
     def _on_message(self, ws, message):
-        print(message)
+        msg = json.loads(message)
+        if msg[1] == "set-promise":
+            self._set_promise(msg[2])
         return
 
+    def _set_promise(self, value):
+        if value["success"]:
+            self.Promises.complete(value["promiseId"], value["value"])
+        else:
+            self.Promises.fail(value["promiseId"], value["error"])
+
     def _on_error(self, ws, error):
+        print(error)
         return
 
     def _on_close(self, ws, close_status_code, close_msg):
+        self.Connected = False
         return
 
 
-if __name__ == "__main__":
+async def main():
     websocket.enableTrace(True)
     client = RaidToolkitClient()
     client.connect()
-    client.call("account-api", "getAccounts")
+    result = await client.call("account-api", "getAccounts")
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
