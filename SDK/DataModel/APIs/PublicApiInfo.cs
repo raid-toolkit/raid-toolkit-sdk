1292f15b270b157b9ff9855884d9c8d3a6e6a703
Daniel Chattan <dnchattan@gmail.com>
2022-01-17 17:26:33 -0500

    fix client interop library

--- SDK/DataModel/APIs/PublicApiInfo.cs
+++ SDK/DataModel/APIs/PublicApiInfo.cs
@@ -8,20 +8,22 @@ namespace Raid.DataModel
     public class PublicApiInfo<T>
     {
         public readonly string Name;
-        public readonly IReadOnlyDictionary<string, ApiMemberDefinition> Members;
+        protected readonly IReadOnlyDictionary<string, ApiMemberDefinition> PublicApiByPublicName;
+        protected readonly IReadOnlyDictionary<string, ApiMemberDefinition> PublicApiByMemberName;
 
         public PublicApiInfo()
         {
             Name = typeof(T).GetCustomAttribute<PublicApiAttribute>().Name;
-            Members = typeof(T).GetMembers()
+            var memberDefinitions = typeof(T).GetMembers()
                     .Select(member => new ApiMemberDefinition(Name, member, member.GetCustomAttribute<PublicApiAttribute>()))
-                    .Where(member => member.Attribute != null)
-                    .ToDictionary(entry => entry.PublicName);
+                    .Where(member => member.Attribute != null);
+            PublicApiByPublicName = memberDefinitions.ToDictionary(entry => entry.PublicName);
+            PublicApiByMemberName = memberDefinitions.ToDictionary(entry => entry.MemberInfo.Name);
         }
 
         public U GetPublicApi<U>(string name, out string scope) where U : MemberInfo
         {
-            if (Members.TryGetValue(name, out ApiMemberDefinition member) && member.MemberInfo is U result)
+            if (PublicApiByPublicName.TryGetValue(name, out ApiMemberDefinition member) && member.MemberInfo is U result)
             {
                 scope = member.Scope;
                 return result;
@@ -31,7 +33,7 @@ namespace Raid.DataModel
 
         public ApiMemberDefinition GetMember<U>(string memberName) where U : MemberInfo
         {
-            return Members.TryGetValue(memberName, out ApiMemberDefinition member) && member.MemberInfo is U
+            return PublicApiByMemberName.TryGetValue(memberName, out ApiMemberDefinition member) && member.MemberInfo is U
                 ? member
                 : throw new MissingMethodException(member?.Scope ?? "", memberName);
         }
