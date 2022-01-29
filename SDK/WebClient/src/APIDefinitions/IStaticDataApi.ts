3b4e394d375a40f95d4f5d1ed28109623e2500fc
Daniel Chattan <dnchattan@gmail.com>
2022-01-28 22:58:22 -0500

    update typings for static data

--- SDK/WebClient/src/APIDefinitions/IStaticDataApi.ts
+++ SDK/WebClient/src/APIDefinitions/IStaticDataApi.ts
@@ -1,26 +1,43 @@
 import { ApiDefinition, methodStub } from '@remote-ioc/runtime';
+import { ArenaLeague, ArenaLeagueId } from '../Types/Arena';
+import { ArtifactSetKind, ArtifactSetKindId } from '../Types/Artifacts';
+import { HeroType } from '../Types/HeroType';
+import { SkillType } from '../Types/SkillType';
+import { AreaData, RegionData, StageData } from '../Types/Stages';
 
+export interface StaticData {
+  localizedStrings: Record<string, string>;
+  heroTypes: Record<number, HeroType>;
+  skillTypes: Record<number, SkillType>;
+  stageData: {
+    areas: Record<string, AreaData>;
+    regions: Record<string, RegionData>;
+    stages: Record<string, StageData>;
+  };
+  arenaData: { leagues: Record<ArenaLeagueId, ArenaLeague> };
+  artifactData: { setKinds: Record<ArtifactSetKindId, ArtifactSetKind> };
+}
 @ApiDefinition('static-data')
 export class IStaticDataApi {
   public async getAllData(): Promise<unknown> {
     methodStub(this);
   }
-  public async getLocalizedStrings(): Promise<unknown> {
+  public async getLocalizedStrings(): Promise<Pick<StaticData, 'localizedStrings'>> {
     methodStub(this);
   }
-  public async getArenaData(): Promise<unknown> {
+  public async getArenaData(): Promise<Pick<StaticData, 'arenaData'>> {
     methodStub(this);
   }
-  public async getArtifactData(): Promise<unknown> {
+  public async getArtifactData(): Promise<Pick<StaticData, 'artifactData'>> {
     methodStub(this);
   }
-  public async getHeroData(): Promise<unknown> {
+  public async getHeroData(): Promise<Pick<StaticData, 'heroTypes'>> {
     methodStub(this);
   }
-  public async getSkillData(): Promise<unknown> {
+  public async getSkillData(): Promise<Pick<StaticData, 'skillTypes'>> {
     methodStub(this);
   }
-  public async getStageData(): Promise<unknown> {
+  public async getStageData(): Promise<Pick<StaticData, 'stageData'>> {
     methodStub(this);
   }
 }
