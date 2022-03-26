241b93b6dcf771aaf8ccd90679c15432e2372d2b
Daniel Chattan <dnchattan@gmail.com>
2022-03-26 17:18:25 -0400

    fix stale data

--- src/Shared/Extensibility/DataSpec.cs
+++ src/Shared/Extensibility/DataSpec.cs
@@ -5,17 +5,17 @@ namespace Raid.Toolkit.Extension
 {
     public class DataSpec<T> where T : class
     {
-        private T Value;
-        private string Key;
+        private readonly string Key;
         public DataSpec(string key)
         {
             Key = key;
         }
         public T Get(CachedDataStorage<PersistedDataStorage> storage, IDataContext context)
         {
-            if (Value == null)
-                storage.TryRead<T>(context, Key, out Value);
-            return Value ?? throw new System.NullReferenceException("Could not obtain value");
+            if (!storage.TryRead<T>(context, Key, out T Value))
+                throw new System.NullReferenceException("Could not obtain value");
+
+            return Value;
         }
     }
 }
