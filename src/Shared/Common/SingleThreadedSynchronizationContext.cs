a170db349c154dd54dd0915111dabfab165cbe3f
Daniel Chattan <dnchattan@gmail.com>
2022-04-13 22:18:13 -0400

    fix installer

---
+++ src/Shared/Common/SingleThreadedSynchronizationContext.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Raid.Toolkit.Common
+{
+    public sealed class SingleThreadedSynchronizationContext : SynchronizationContext
+    {
+        private readonly BlockingCollection<(SendOrPostCallback d, object state)> queue = new();
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            queue.Add((d, state));
+        }
+
+        public static void Await(Func<Task> taskinvoker)
+        {
+            var originalContext = Current;
+            try
+            {
+                var context = new SingleThreadedSynchronizationContext();
+                SetSynchronizationContext(context);
+
+                var task = taskinvoker.Invoke();
+                task.ContinueWith(_ => context.queue.CompleteAdding());
+
+                while (context.queue.TryTake(out var work, Timeout.Infinite))
+                    work.d.Invoke(work.state);
+
+                task.GetAwaiter().GetResult();
+            }
+            finally
+            {
+                SetSynchronizationContext(originalContext);
+            }
+        }
+    }
+}
