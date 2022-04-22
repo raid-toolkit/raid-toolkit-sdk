3858267af73b3abe9c8f3e30b3c0f7fe535ca80b
Daniel Chattan <dnchattan@gmail.com>
2022-04-21 22:09:46 -0400

    fix extension dll load strategy

--- src/Shared/Extensibility.Host/Implementations/ExtensionLoadContext.cs
+++ src/Shared/Extensibility.Host/Implementations/ExtensionLoadContext.cs
@@ -8,23 +8,31 @@ namespace Raid.Toolkit.Extensibility.Host
 {
     public class ExtensionLoadContext : AssemblyLoadContext
     {
-        private readonly AssemblyDependencyResolver[] Resolvers;
-
+        private readonly string[] SearchPaths;
         public ExtensionLoadContext(string pluginPath) : base(isCollectible: true)
         {
-            Resolvers = new AssemblyDependencyResolver[] {
-                new AssemblyDependencyResolver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)),
-                new AssemblyDependencyResolver(Path.GetDirectoryName(pluginPath))
+            SearchPaths = new string[] {
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                pluginPath
             };
         }
 
         protected override Assembly Load(AssemblyName assemblyName)
         {
-            foreach(var resolver in Resolvers)
+            if (assemblyName.Name == "Raid.Interop")
+                return null;
+
+            Assembly alreadyLoaded = Default.Assemblies.FirstOrDefault(asm => asm.GetName().Name == assemblyName.Name);
+            if (alreadyLoaded != null)
+                return alreadyLoaded;
+
+            foreach (var searchPath in SearchPaths)
             {
-                string assemblyPath = resolver.ResolveAssemblyToPath(assemblyName);
-                if (assemblyPath == null) continue;
-                return LoadFromAssemblyPath(assemblyPath);
+                string testPath = Path.Combine(searchPath, $"{assemblyName.Name}.dll");
+                if (File.Exists(testPath))
+                {
+                    return Default.LoadFromAssemblyPath(testPath);
+                }
             }
 
             return null;
@@ -32,12 +40,12 @@ namespace Raid.Toolkit.Extensibility.Host
 
         protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
         {
-            foreach (var resolver in Resolvers)
-            {
-                string libraryPath = resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
-                if (libraryPath == null) continue;
-                return LoadUnmanagedDllFromPath(libraryPath);
-            }
+            //foreach (var resolver in Resolvers)
+            //{
+            //    string libraryPath = resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            //    if (libraryPath == null) continue;
+            //    return LoadUnmanagedDllFromPath(libraryPath);
+            //}
 
             return IntPtr.Zero;
         }
