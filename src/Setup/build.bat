ec694c07fcd43707e2e0ee44dc422de4dc4d974a
Daniel Chattan <dnchattan@gmail.com>
2022-10-06 21:59:08 -0400

    update setup

--- src/Setup/build.bat
+++ src/Setup/build.bat
@@ -2,11 +2,13 @@
 
 mkdir %~dp0out 2>nul
 
-msiexec /x out\RaidToolkit.msi
+:: msiexec /x out\RaidToolkit.msi
 
-call yarn build publish --flavor Release
+set BuildOutputDirectory=..\Application\Raid.Toolkit\bin\x64\Release\net6.0-windows10.0.19041.0\win10-x64
+
+heat.exe dir %BuildOutputDirectory% -cg RaidToolkitBinaries -out RaidToolkitBinaries.wxs -dr INSTALLDIR -srd -suid -sreg -g1 -var env.BuildOutputDirectory -gg
 
 candle.exe -nologo -o %~dp0out\ %~dp0RaidToolkitBinaries.wxs %~dp0Product.wxs
 light.exe -nologo -o %~dp0out\RaidToolkit.msi %~dp0out\Product.wixlib %~dp0out\RaidToolkitBinaries.wixobj
 
-msiexec /i %~dp0out\RaidToolkit.msi /log %~dp0out\install.log
+:: msiexec /i %~dp0out\RaidToolkit.msi /log %~dp0out\install.log
