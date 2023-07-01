4d1e8427ed323b85cafb5e223122a635393b3ae6
Daniel Chattan <dnchattan@gmail.com>
2023-07-01 14:48:30 -0400

    add template to SDK repo

---
+++ src/Template/src/Extension.cs
@@ -0,0 +1,13 @@
+﻿using Raid.Toolkit.Extensibility;
+using System;
+
+namespace $safeprojectname$
+{
+	public class Extension : ExtensionPackage
+	{
+		public override void OnActivate(IExtensionHost host)
+		{
+			// TODO: Initialize your extension here
+		}
+	}
+}
